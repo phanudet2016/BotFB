3042f8a3fccd20cc01188de4d89c879ccbf0c490
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-12 12:35:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ function receivedMessage(event) {
 
   if (messageText) {
     if (messageText === 'hello') {
-      sendTextMessage(senderID, "ควยเอ้ย ไม่รู้ request");
+      sendTextMessage(senderID, "สวัสดีจร้า");
     }
 
     // If we receive a text message, check to see if it matches a keyword
