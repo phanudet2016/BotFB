7c8c7b3c7313b0b47532bfda9c5faba442837bc8
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-16 20:06:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -57,15 +57,30 @@ function receivedMessage(event) {
   console.log(JSON.stringify(message));
 
   var messageId = message.mid;
-
+  var isEcho = message.is_echo;
+  var appId = message.app_id;
+  var metadata = message.metadata;
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
+  if (isEcho) {
+    // Just logging message echoes to console
+    console.log("Received echo for message %s and app %d with metadata %s", 
+      messageId, appId, metadata);
+    return;
+  } else if (quickReply) {
+    var quickReplyPayload = quickReply.payload;
+    console.log("Quick reply for message %s with payload %s",
+      messageId, quickReplyPayload);
+
+    sendTextMessage(senderID, "Quick reply tapped");
+    return;
+  }
+  
   if (messageText) {
-    if (messageText === "Call Postback") {
+    if (messageText === 'hello') {
       sendTextMessage(senderID, "สวัสดีจร้า");
     }
-
     // If we receive a text message, check to see if it matches a keyword
     // and send back the example. Otherwise, just echo the text we received.
     switch (messageText) {
