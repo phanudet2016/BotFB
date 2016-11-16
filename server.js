b98a8ca7814f873b0558c94bc610e39cc7a5dbf4
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-17 00:18:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,31 @@ function receivedMessage(event) {
   }
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+  if(){
+  }
+  else if(payload == 'findRestaurant'){
+    var result = "";
+  }
+  else if(payload == 'noThank'){
+    var result = "แน่ใจนะครับ! คุณจะไม่หิวตอนนี้ใช่มั้ยครับ :(";
+  }
+  else{}
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, result);
+}
 
 
 function sendGenericMessage(recipientId, messageText) {
