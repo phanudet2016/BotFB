c0eb78b59f98711712d17a32be7abc187d52ff38
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-17 01:20:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -97,8 +97,9 @@ function receivedPostback(event) {
     var result = "gg";
   }
   else if(payload == 'noThank'){
+    var timer1 =setTimeout(function() { alert(“Test”); }, 1000);
     sendTextMessage(senderID, "หากคุณต้องการมองหาร้านอาหารในปราจีนบุรีอีก เพียงแค่ให้ผมช่วย")
-    sleep(2000);
+    timer1
     sendTextMessage(senderID, "แน่ใจนะครับ! คุณจะไม่หิวตอนนี้ใช่มั้ยครับ")
     sendTextMessage(senderID, ":(")
   } else {
