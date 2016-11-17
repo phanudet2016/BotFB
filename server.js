8985371477b4762ec009ece2e61c98a50f9e651d
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-17 14:09:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -57,10 +57,29 @@ function receivedMessage(event) {
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
+  var isEcho = message.is_echo;
   var messageId = message.mid;
+  var appId = message.app_id;
+  var metadata = message.metadata;
 
+  // You may get a text or attachment but not both
   var messageText = message.text;
   var messageAttachments = message.attachments;
+  var quickReply = message.quick_reply;
+
+  if (isEcho) {
+    // Just logging message echoes to console
+    console.log("Received echo for message %s and app %d with metadata %s", 
+      messageId, appId, metadata);
+    return;
+  } else if (quickReply) {
+    var quickReplyPayload = quickReply.payload;
+    console.log("Quick reply for message %s with payload %s",
+      messageId, quickReplyPayload);
+
+    sendTextMessage(senderID, "Quick reply tapped");
+    return;
+  }
 
   if (messageText) {
     if (messageText === 'hello') {
