ff6c6d8973903af91a32c697df2347f78404d1f2
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-17 13:43:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function receivedMessage(event) {
   }
 }
 
-function receivedPostback(event) {
+/*function receivedPostback(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
@@ -108,7 +108,7 @@ function receivedPostback(event) {
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
   // sendTextMessage(senderID, emoji);
-}
+}*/
 
 function sendGreetMessage(recipientId, messageText) {
   var messageData = {
