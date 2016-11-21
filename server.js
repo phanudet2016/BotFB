0a9b73910a561d18ac1a9001ed6df5eed98fa0ae
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-21 20:29:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ function receivedPostback(event) {
     }, 1000)
     setTimeout(function() {
       if(payload == 'eatCafeFirst'){sendImageMessage(senderID)}
-    }, 100)
+    }, 10)
     setTimeout(function() {
       if(payload == 'eatCafeFirst'){sendTextMessage(senderID, "ฮันนี่ โทสต์");}
     }, 2000)
