6e253642fb186cfa0a50ff8a07f7afa25fb9680a
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2016-11-22 22:03:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -563,14 +563,11 @@ function receivedPostback(event) {
   /////////////แสดงรายละเอียด///////////////
   else if(payload=='detailPalmFirst'){
     setTimeout(function() {
-        if(payload=='detailPalmFirst'){sendTextMessage(senderID, "นี้คือแผ่นที่ร้าน ที่จะพาคุณไปยังร้านอาหารสิ่งนี้ https://www.google.co.th/maps/place/%E0%B8%9B%E0%B8%B2%E0%B8%A5%E0%B9%8C%E0%B8%A1%E0%B8%AA%E0%B8%A7%E0%B8%B5%E0%B8%97%E0%B9%82%E0%B8%AE%E0%B8%A1+(Palm+sweet+home)/@13.9831288,101.7684302,15z/data=!4m5!3m4!1s0x0:0x530a91dc0a6a290!8m2!3d13.9831288!4d101.7684302");}
+        if(payload=='detailPalmFirst'){sendTextMessage(senderID, "นี้คือสิ่งที่คุณจะไป 🏠");}
       }, 500)
       setTimeout(function() {
-        if(payload=='detailPalmFirst'){sendTextMessage(senderID, "นี้คือสิ่งที่คุณจะไป 🏠");}
-      }, 1000)
-      setTimeout(function() {
         if(payload=='detailPalmFirst'){detailPalmFirst(senderID);}
-      }, 1500)
+      }, 1000)
   }
   else {
     var result = "";
@@ -597,13 +594,18 @@ function detailPalmFirst(recipientId, messageText) {
             buttons:[
               {
                 type:"postback",
-                title:"✅ แน่นอน! ฉันจะไปที่นี้",
+                title:"✅ แน่นอน!ฉันจะไปที่นี้",
                 payload:"surePalmFirst"
               },
               {
                 type:"postback",
-                title:"🐧 รีวิวร้าน",
+                title:"📖 รีวิวร้าน",
                 payload:"reviewPalmFirst"
+              },
+              {
+                type:"web_url",
+                url:"https://www.google.co.th/maps/place/%E0%B8%9B%E0%B8%B2%E0%B8%A5%E0%B9%8C%E0%B8%A1%E0%B8%AA%E0%B8%A7%E0%B8%B5%E0%B8%97%E0%B9%82%E0%B8%AE%E0%B8%A1+(Palm+sweet+home)/@13.9831288,101.7684302,15z/data=!4m5!3m4!1s0x0:0x530a91dc0a6a290!8m2!3d13.9831288!4d101.7684302",
+                title:"🎯 แผ่นที่"
               },
               {
                 type:"postback",
